561daa77d72d5cb84a4892ec62e1edb211d70ba6
Jorge Rebosio <jorge.rebosio@utec.edu.pe>
2019-11-27 15:39:21 -0500

    code2

--- Client/code2/main.cpp
+++ Client/code2/main.cpp
@@ -5,13 +5,7 @@
 int main() {
     build p;
     p.Handshake();
-/*
-    filesystem::create_directory(filesystem::current_path()/ "../../in");
-    filesystem::path in=filesystem::current_path()/"../../in";
-    fstream myfile(in/"Primero.txt",ios::out);
-    myfile<<"HANDSHAKE=LosVengadores";
-    myfile.close();
-    */
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
